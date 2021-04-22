8dae44cf472799fe8b5699ee77a37cfdaafc63ce
Pedro Torres <pedrohagatorres@gmail.com>
2021-04-22 14:17:48 -0300

    Initial commit for program counter modules.

---
+++ pc_plus_four.v
@@ -0,0 +1,10 @@
+module pcPlusFour (clk, pc, out);
+	input      clk;
+   input      [31:0] pc;
+   output	  [31:0] out;
+   reg        [31:0] pc;
+    // Register with active-high clock
+   always @ (posedge clk)
+      out <= pc + 4;
+    
+endmodule	// programCounter32
