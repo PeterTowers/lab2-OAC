83574303c52fcd29c007c7799d7134164ad0aee8
Pedro Torres <pedrohagatorres@gmail.com>
2021-04-26 10:26:19 -0300

    Modified PC module to receive 'pc_src' as a 2 bit bus.
    Instead of 3 different variables to determine what the next PC would be.

--- program_counter.v
+++ program_counter.v
@@ -3,8 +3,8 @@
  * (PC), increasing it at each cycle, branch and jump addresses calculation.
  */
 module program_counter(
-	input		clk,								// Clock
-	input		branch, alu_zero, jump,	jr,// Control signals for branch and jump
+	input		clk, alu_zero,					// Clock & signal from ALU
+	input		[1:0]  pc_src,					// Source signal for PC (j, jr, branch)
 	input    [31:0] b_address, reg_addr,// Branch & register addresses
 	input    [25:0] j_address,				// Jump address
 	output	[31:0] out						// Output bus
@@ -25,16 +25,19 @@ module program_counter(
    always @(*) begin
 		pc = pc + 1;	// PC always increases by 1
 		
-		if (branch & alu_zero) begin	// If there's a branch PC = PC + b_address*4
+		// Branch: occurs when pc_src = 2'b00 and alu_zero is set to 1
+		if (!pc_src[1] & !pc_src[0] & alu_zero) begin 
 			pc = pc + (b_address << 2);	// Shift left as per how it's implemented
 		end
 		
-		else if (jump) begin				// Unconditional absolute jump
+		// Unconditional absolute jump (j, jal): occurs when pc_src = 2'b01
+		else if (!pc_src[1] & pc_src[0]) begin
 			sll_j_address = j_address << 2;	// Jump address is shifted left by 2 bits
 			pc = pc[31:28] + sll_j_address[27:0];	// PC receives its 4 MSB and the
 		end													// others come from given adress
 		
-		else if (jr) begin	// Unconditional register-indirect jump
+		// Unconditional register-indirect jump (jr, jalr): occurs when pc_src = 2'b10
+		else if (pc_src[1] & !pc_src[0]) begin
 			pc = reg_addr;		// PC is set to received address
 		end
     end
--- program_counter_tb.v
+++ program_counter_tb.v
@@ -3,7 +3,8 @@
 module program_counter_tb;
 
 	// Inputs
-	reg	clk, branch, alu_zero, jump, jr;
+	reg	clk, alu_zero;
+	reg	[1:0]  pc_src;
 	reg	[31:0] b_address, reg_addr;
 	reg	[25:0] j_address;
 	wire	[31:0] out;
@@ -12,10 +13,8 @@ module program_counter_tb;
 	
 	program_counter test_unit(
 		.clk(clk), 
-		.branch(branch),
 		.alu_zero(alu_zero),
-		.jump(jump),
-		.jr(jr),
+		.pc_src(pc_src),
 		.b_address(b_address),
 		.j_address(j_address),
 		.reg_addr(reg_addr),
@@ -26,7 +25,7 @@ module program_counter_tb;
 		input [31:0] expected_value;
 		begin			
 			if (out == expected_value) 
-				$display("correct");
+				$display("correct\n");
 			else  
 				$display("INCORRECT! Expected: %0d; Got: %0d", expected_value, out);
 		end
@@ -46,76 +45,80 @@ module program_counter_tb;
 	 * to 1. In that case, PC = PC + address*4.
 	 */
 	initial begin
-		branch = 0;
-		alu_zero = 0;
-		jump = 0;
-		b_address = 32'bx;
-		j_address = 26'bx;
+		alu_zero = 0;			// LO signal from ALU
+		pc_src = 2'b11;		// pc_src = 2'b11 (src = PC+1)
+		b_address = 32'bx;	// Doesn't matter
+		j_address = 26'bx;	// Doesn't matter
+		reg_addr = 32'bx;		// Doesn't matter
 		#period;
 		
 		$display("Out: %0d\nBranch address: %0d", out, b_address);
-		$display("Branch: %0d, Alu_zero: %0d", branch, alu_zero);
+		$display("Jump address: %0d\nJump reg. address: %0d", j_address, reg_addr);
+		$display("pc_src: %2b, Alu_zero: %0d", pc_src, alu_zero);
 		test_result(0);	// First instruction, starts at 0
 		#period;
 		
-		branch = 0;
-		alu_zero = 1;
-		jump = 0;
-		b_address = 32'bx;
-		j_address = 26'bx;
+		alu_zero = 1;			// HI signal from ALU
+		pc_src = 2'b11;		// But no branch signal from control
+		b_address = 32'bx;	// Doesn't matter
+		j_address = 26'bx;	// Doesn't matter
+		reg_addr = 32'bx;		// Doesn't matter
 		#period;
 		
 		$display("Out: %0d\nBranch address: %0d", out, b_address);
-		$display("Branch: %0d, Alu_zero: %0d", branch, alu_zero);
-		test_result(1);	// Only alu_zero is set, so PC+1 = 4
+		$display("Jump address: %0d\nJump reg. address: %0d", j_address, reg_addr);
+		$display("pc_src: %2b, Alu_zero: %0d", pc_src, alu_zero);
+		test_result(1);	// Only alu_zero is set, so PC+1 = 1
 		#period;
 		
-		branch = 1;
-		alu_zero = 0;
-		jump = 0;
-		b_address = 32'bx;
-		j_address = 26'bx;
-		#period;
-		
-		$display("Out: %0d\nBranch address: %0d", out, b_address);
-		$display("Branch: %0d, Alu_zero: %0d", branch, alu_zero);
-		test_result(2);	// Now, only branch is set, so PC+1 = 8
-		#period;
-		
-		branch = 1;
-		alu_zero = 1;
-		jump = 0;
-		b_address = 32'b1010;
-		j_address = 26'bx;
+		alu_zero = 0;			// LO signal from ALU
+		pc_src = 2'b00;		// pc_src indicates branch (2'b00)
+		b_address = 32'bx;	// Doesn't matter
+		j_address = 26'bx;	// Doesn't matter
+		reg_addr = 32'bx;		// Doesn't matter
 		#period;
 		
 		$display("Out: %0d\nBranch address: %0d", out, b_address);
-		$display("Branch: %0d, Alu_zero: %0d", branch, alu_zero);
-		test_result(43);	// Both variables are set, so PC = PC + address*4 = 43
-		
-		branch = 0;
-		alu_zero = 0;
-		jump = 1;
-		b_address = 32'bx;
-		j_address = 26'b1111;
+		$display("Jump address: %0d\nJump reg. address: %0d", j_address, reg_addr);
+		$display("pc_src: %2b, Alu_zero: %0d", pc_src, alu_zero);
+		test_result(2);	// Branch is set, but not eq. or ineq. so PC+1 = 2
 		#period;
 		
-		$display("Out: %0d\nJump address: %0d", out, j_address);
-		$display("Branch: %0d, Alu_zero: %0d", branch, alu_zero);
-		test_result(60);	// Both variables are set, so PC = PC + address*4 = 43
-		
-		branch = 0;
-		alu_zero = 0;
-		jump = 0;
-		jr = 1;
-		b_address = 32'bx;
-		j_address = 26'b1111;
-		reg_addr = 32'b1101_1110_1010_1101_1011_1110_1110_1111;
+		alu_zero = 1;				// HI signal from ALU (condition met)
+		pc_src = 2'b00;			// pc_src indicates branch (2'b00)
+		b_address = 32'b1010;	// Address for branch is set to 32'b1010 (d10)
+		j_address = 26'bx;		// Doesn't matter
+		reg_addr = 32'bx;			// Doesn't matter
 		#period;
 		
-		$display("Out: %0d\nJump address: %0d", out, j_address);
-		$display("Branch: %0d, Alu_zero: %0d", branch, alu_zero);
-		test_result(32'hdeadbeef);	// Both variables are set, so PC = PC + address*4 = 43
+		$display("Out: %0d\nBranch address: %0d", out, b_address);
+		$display("Jump address: %0d\nJump reg. address: %0d", j_address, reg_addr);
+		$display("pc_src: %2b, Alu_zero: %0d", pc_src, alu_zero);
+		test_result(43);	// Branch is set & condition is met, so PC = PC + address*4 = 43
+		
+		alu_zero = 0;				// LO signal from ALU
+		pc_src = 2'b01;			// pc_src is set for jump (j/jal)
+		b_address = 32'bx;		// Doesn't matter
+		j_address = 26'b1111;	// Address for jump is set to 32'b1111 (d15)
+		reg_addr = 32'bx;			// Doesn't matter
+		#period;
+		
+		$display("Out: %0d\nBranch address: %0d", out, b_address);
+		$display("Jump address: %0d\nJump reg. address: %0d", j_address, reg_addr);
+		$display("pc_src: %2b, Alu_zero: %0d", pc_src, alu_zero);
+		test_result(60);	// pc_src is set for jump, so PC = address*4 = 60
+		
+		alu_zero = 0;				// LO signal from ALU
+		pc_src = 2'b10;			// pc_src is set for jr/jalr
+		b_address = 32'bx;		// Doesn't matter
+		j_address = 26'bx;		// Doesn't matter
+		reg_addr = 32'b1101_1110_1010_1101_1011_1110_1110_1111; // Address set in register
+		#period;
+		
+		$display("Out: %0d\nBranch address: %0d", out, b_address);
+		$display("Jump address: %0d\nJump reg. address: %0d", j_address, reg_addr);
+		$display("pc_src: %2b, Alu_zero: %0d", pc_src, alu_zero);
+		test_result(32'hdeadbeef);	// pc_src is set for jr/jalr, so PC = reg_addr
 	end
 	
 endmodule
