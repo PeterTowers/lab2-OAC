3679d50e7d39548313d304d63fab12f06eb26484
Pedro Torres <pedrohagatorres@gmail.com>
2021-04-26 14:54:09 -0300

    Added 3rd condition/selection.

--- mux1_5bits.v
+++ mux1_5bits.v
@@ -2,17 +2,19 @@ module mux1_5bits
 	(
 		input [4:0] option_a,
 		input [4:0] option_b,
-		input selector,
+		input [1:0] selector,
 		output reg [4:0] out
 	); 
 	
 	always @*
 	begin
 		case(selector)
-			1'b0:	
+			2'b00:	
 				out <= option_a;
-			1'b1: 
+			2'b01: 
 				out <= option_b;
+			2'b10:
+				out <= 5'd31;
 		endcase
 	end
 	 
