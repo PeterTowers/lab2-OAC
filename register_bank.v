de537b2527e91b000aa79928fa00a5eba4bd20cf
Pedro Torres <pedrohagatorres@gmail.com>
2021-04-28 19:52:35 -0300

    Changed write logic.
    To occur, write needs to be enable by control unit AND enabled by MUU control unit AND register must not be '$zero'.

--- register_bank.v
+++ register_bank.v
@@ -1,7 +1,7 @@
 module register_bank(
 	input clock,
 	input[4:0] read_reg1, read_reg2, write_reg,	
-	input write_enable,
+	input write_enable, muu_write_enable,
 	input [31:0] write_data,
 	output reg[31:0] read_data1, read_data2
 	);
@@ -17,9 +17,9 @@ module register_bank(
 	end
 	
 	//Sempre que entrar a borda de subida do clock
-	always @(posedge clock)
-	begin
-		if(write_enable) // Se o write_enable for 1				
+	always @(posedge clock) begin
+		// Para realizar escrita, write_enable = 1, muu_write_enable = 1 e reg != $zero
+		if(write_enable && muu_write_enable && (write_reg != 0) )
 			registers[write_reg] <= write_data; //Escreve no registrador indifcado por write_reg
 	end		
 	always @*
